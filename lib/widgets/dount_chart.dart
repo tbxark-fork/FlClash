eb410a404288a65e33dda6bcc14d0b20f9a3dbf2
chen08209 <fengchen@FengdeMacBook-Pro.local>
2025-01-02 15:43:43 +0800

    cache

--- lib/widgets/dount_chart.dart
+++ lib/widgets/dount_chart.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:fl_clash/common/common.dart';
 import 'package:flutter/material.dart';
 
 @immutable
@@ -14,26 +15,87 @@ class DonutChartData {
   double get value => _value;
 }
 
-class DonutChart extends StatelessWidget {
+class DonutChart extends StatefulWidget {
   final List<DonutChartData> data;
+  final Duration duration;
 
   const DonutChart({
     super.key,
     required this.data,
+    this.duration = commonDuration,
   });
 
   @override
+  State<DonutChart> createState() => _DonutChartState();
+}
+
+class _DonutChartState extends State<DonutChart> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late List<DonutChartData> _oldData;
+
+  @override
+  void initState() {
+    super.initState();
+    _oldData = widget.data;
+    _animationController = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    )..forward();
+  }
+
+  @override
+  void didUpdateWidget(DonutChart oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.data != widget.data) {
+      _oldData = oldWidget.data;
+      _animationController.forward(from: 0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: DonutChartPainter(data),
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return CustomPaint(
+          painter: DonutChartPainter(
+            _oldData,
+            widget.data,
+            _animationController.value,
+          ),
+        );
+      },
     );
   }
 }
 
 class DonutChartPainter extends CustomPainter {
-  final List<DonutChartData> data;
+  final List<DonutChartData> oldData;
+  final List<DonutChartData> newData;
+  final double progress;
 
-  DonutChartPainter(this.data);
+  DonutChartPainter(this.oldData, this.newData, this.progress);
+
+  List<DonutChartData> get interpolatedData {
+    if (oldData.length != newData.length) return newData;
+
+    return List.generate(newData.length, (index) {
+      final oldValue = oldData[index].value;
+      final newValue = newData[index].value;
+      final interpolatedValue = oldValue + (newValue - oldValue) * progress;
+
+      return DonutChartData(
+        value: interpolatedValue,
+        color: Color.lerp(oldData[index].color, newData[index].color, progress)!,
+      );
+    });
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -43,9 +105,10 @@ class DonutChartPainter extends CustomPainter {
 
     double gapAngle = 2 * asin(strokeWidth * 1 / (2 * radius)) * 1.2;
 
+    final data = interpolatedData;
     final total = data.fold<double>(
       0,
-      (sum, item) => sum + item.value,
+          (sum, item) => sum + item.value,
     );
 
     final availableAngle = 2 * pi - (data.length * gapAngle);
@@ -75,6 +138,8 @@ class DonutChartPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(DonutChartPainter oldDelegate) {
-    return true;
+    return oldDelegate.progress != progress ||
+        oldDelegate.oldData != oldData ||
+        oldDelegate.newData != newData;
   }
 }
